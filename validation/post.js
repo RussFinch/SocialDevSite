d5ae62cdc04a1d62a466cef67b7582d8e41bbffc
Russ <rfincham@hotmail.co.uk>
2019-02-03 11:53:10 +0000

    Post create route and validate

---
+++ validation/post.js
@@ -0,0 +1,26 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  // Conditional Ternary operator, if not empty data.text = data.text
+  // else data.text = ''
+  data.text = !isEmpty(data.text) ? data.text : '';
+
+  if (!validator.isLength(data.text, {
+      min: 10,
+      max: 300
+    })) {
+    errors.text = 'Post must be between 10 and 300 characters';
+  }
+
+  if (validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
